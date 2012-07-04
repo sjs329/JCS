21201a7178265540aa150c4ffca3f6120e3b157f
Scott <sjs329@cornell.edu>
2012-07-04 17:59:14 -0400

    added occupation (incomplete)

---
+++ src/Banking_System/Occupation.java
@@ -0,0 +1,16 @@
+package Banking_System;
+
+public class Occupation {
+	//Fields
+	private Company company;
+	private String position;
+	private double wage;
+	
+	
+	
+	public Occupation(Company company, String position, double wage){
+		this.company = company;
+		this.position = position;
+		this.wage = wage;
+	}
+}
